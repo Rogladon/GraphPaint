f87bad3ea751c93dc52cb564633dc615221a49e0
Александр Бахаревский <bakharevskiya@mail.ru>
2022-03-03 17:10:06 +0300

    add(Utilits): asmdef, Creator.cs

---
+++ Assets/Utilits/Creator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+public class Creator {
+	public static T Create<T>() where T: MonoBehaviour {
+		return new GameObject($"{typeof(T).Name}").AddComponent<T>();
+	}
+}
