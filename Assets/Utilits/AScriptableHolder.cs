2f0a9c0111a7d715483ae50cff695029bd49ed27
Александр Бахаревский <bakharevskiya@mail.ru>
2022-03-05 17:05:17 +0300

    add(Utilits): ScriptableHolder

---
+++ Assets/Utilits/AScriptableHolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Абстрактный холдер для ScriptableObject, имеет только свойство возвращающее object
+/// </summary>
+public abstract class AScriptableHolder : ScriptableObject {
+	public abstract object obj { get; }
+}
