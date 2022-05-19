0cf4c7482b0f6e98fd33b5dc3e1bce4eae08705f
Александр Бахаревский <bakharevskiya@mail.ru>
2022-05-19 16:58:08 +0300

    add(Manager): FileManager

---
+++ Assets/Components/Manager/Scripts/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+using Components.Graphs;
+using UnityEngine;
+
+namespace Components.Manager {
+	public class FileManager {
+		private Action<Graph> change;
+		private Func<Graph> graph;
+		public FileManager(Func<Graph> graph, Action<Graph> change) {
+			this.graph = graph;
+			this.change = change;
+		}
+
+		public void Open(string path) {
+			if(!File.Exists(path)) {
+				Debug.LogError($"FileManager: Don`t exist path: {path}");
+				return;
+			}
+			StreamReader stream = new StreamReader(path);
+			string str = stream.ReadToEnd();
+			stream.Close();
+			var g = new Graphs.File.ConvertColToGraph().Execute(str);
+			change(g);
+		}
+	}
+}
