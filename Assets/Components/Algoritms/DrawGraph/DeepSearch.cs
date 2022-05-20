a787165ec0ec8e1441b1763a87c2b3fddd6c7363
Александр Бахаревский <bakharevskiya@mail.ru>
2022-05-20 16:43:13 +0300

    fix(Algoritm): исправлен поиск в глубину

--- Assets/Components/Algoritms/DrawGraph/DeepSearch.cs
+++ Assets/Components/Algoritms/DrawGraph/DeepSearch.cs
@@ -9,38 +9,72 @@ using node = Components.Algoritm.DrawGraph.Utilits.Node;
 
 namespace Components.Algoritm.DrawGraph {
 	public class DeepSearch : IAlgoritmDraw {
-		public async Task<ResultDraw> Execute(Graph graph) {
-            List<ACommand> commands = new List<ACommand>();
-            List<node> _nodes = new List<node>();
-            foreach (var i in graph.nodes) {
-                _nodes.Add(new node(i));
-                //i.SetColor(-1);
+        private class Vertex {
+            private int id;
+            private Node _node;
+            private List<Vertex> _neiborhood = new List<Vertex>();
+            private int _color = -1;
+
+            public int color => _color;
+            public Node node => _node;
+
+            public Vertex(Node node) {
+                this._node = node;
+                id = node.id;
             }
-            node n = _nodes[0];
-            Stack<node> nodes = new Stack<node>();
-            Stack<node> neighbouringNodes = new Stack<node>();
-            n.color = (0);
-            nodes.Push(n);
-            n._node.neiborhood.ForEach(p => neighbouringNodes.Push(_nodes.First(p1 => p1._node == p)));
-            var colors = Enumerable.Range(0, _nodes.Count);
+            public void SetNeiborhood(List<Vertex> vertices) {
+                _neiborhood.Clear();
+                vertices.Where(p =>
+                    _node.neiborhood.Contains(p._node))
+                    .ForEach(p => _neiborhood.Add(p));
+            }
+            public Vertex PopNeiborhod() {
+                return _neiborhood.FirstOrDefault(p => p.color == -1);
+			}
+            public bool TrySetColor(int color) {
+                if (_color != -1) throw new Exception("Error DeepSerch: повторное назначение цвета");
+                if (_neiborhood.FirstOrDefault(p => p.color == color) != null) return false;
+                _color = color;
+                return true;
+			}
+        }
+
+        public async Task<ResultDraw> Execute(Graph graph) {
+            List<Vertex> vertices = graph.nodes.Select(p => new Vertex(p)).ToList();
+            vertices.ForEach(p => p.SetNeiborhood(vertices));
+            Stack<Vertex> history = new Stack<Vertex>();
             int chromaticNumber = 0;
 
+            Vertex vertex = vertices[0];
             await Task.Run(() => {
-                while (neighbouringNodes.Count != 0) {
-                    n = neighbouringNodes.Pop();
-                    if (nodes.Contains(n)) continue;
-                    nodes.Push(n);
-                    n.color = colors.First(p => !n._node.neiborhood.Select(c => _nodes.First(cc => c == cc._node).color).Contains(p));
-                    chromaticNumber = n.color > chromaticNumber ? n.color : chromaticNumber;
-                    //n.SetColor(colors.First(p => !n.neighbouringNodes.Select(c => graph[c].color).Contains(p)));
-                    neighbouringNodes.Clear();
-                    n._node.neiborhood.ForEach(p => neighbouringNodes.Push(_nodes.First(p1 => p1._node == p)));
-                    //n.neighbouringNodes.ForEach(p => neighbouringNodes.Push(graph[p]));
+                do {
+                    chromaticNumber = Math.Max(chromaticNumber, TrySetFreeColor(vertex));
+
+                    while (vertex.PopNeiborhod() == null) {
+                        if (history.Count == 0) {
+                            vertex = null;
+                            break;
+                        }
+                        vertex = history.Pop();
+                    }
+                    if (vertex != null) {
+                        history.Push(vertex);
+                        vertex = vertex.PopNeiborhod();
+                    }
                 }
+                while (history.Count > 0);
             });
             
 
-            return new ResultDraw(chromaticNumber + 1, new CommandStack(_nodes.Select(p => new Node.CmdSetColor() { owner = p._node, color = p.color }).ToArray()));
+            return new ResultDraw(chromaticNumber, new CommandStack(
+                vertices.Select(p => new Node.CmdSetColor() { owner = p.node, color = p.color }).ToArray()));
         }
+
+        private int TrySetFreeColor(Vertex vertex) {
+            for(int i = 0; i< int.MaxValue; i++) {
+                if (vertex.TrySetColor(i)) return i;
+			}
+            return -1;
+		}
     }
 }
--- Assets/Components/Instruments/Scripts/_instruments/InstrumentAddNode.cs
+++ Assets/Components/Instruments/Scripts/_instruments/InstrumentAddNode.cs
@@ -8,13 +8,16 @@ namespace Components.Instruments._instruments {
 	[System.Serializable]
 	internal class AddNode : IInstrument {
 		private IInstrumentManager mg => InstrumentManager.instance;
+		private bool isStartOnWorkspace = false;
 		public void FinishExecute(Graph graph, Selected selected) {
-			if(PhysicsExtentions.RaycastCameraForTag(out RaycastHit hit, mg.TAG_WORKESPACE))
+			if(PhysicsExtentions.RaycastCameraForTag(out RaycastHit hit, mg.TAG_WORKESPACE) 
+				&& isStartOnWorkspace)
 				new Graph.CmdAddNode { graph = graph, position = hit.point.Vector2() }.Execute();
 		}
 
 		public void StartExecute( Graph graph, Selected selected) {
-			
+			if (PhysicsExtentions.RaycastCameraForTag(out RaycastHit hit, mg.TAG_WORKESPACE))
+				isStartOnWorkspace = true;
 		}
 
 		public void UpdateExecute(Graph graph, Selected selected) {
