1daf3a6ca21385a0adcab44401b7c5dd87f74f4e
Александр Бахаревский <bakharevskiya@mail.ru>
2022-05-21 21:06:27 +0300

    fix(HUD): Select отключает контент при надатии на кнопку контента

--- Assets/Components/HUD/Scripts/Select.cs
+++ Assets/Components/HUD/Scripts/Select.cs
@@ -8,15 +8,21 @@ namespace Components.HUD {
 	public class Select : MonoBehaviour,  ISelectHandler, IDeselectHandler {
 		[SerializeField] private RectTransform content;
 
+		private Button btn;
 		private void Start() {
+			btn = GetComponent<Button>();
+
+			content.gameObject.SetActive(true);
 			GetComponentsInChildren<Button>()
 				.Where(p => p.gameObject != gameObject)
-				.ForEach(p =>
-				p.onClick.AddListener(() => OnDeselect(null)));
+				.ForEach(p => 
+					p.onClick.AddListener(() => OnDeselect(null)));
+			OnDeselect(null);
 		}
 
 		public void OnDeselect(BaseEventData eventData) {
 			content.gameObject.SetActive(false);
+			btn?.OnDeselect(eventData);
 		}
 
 		public void OnSelect(BaseEventData eventData) {
